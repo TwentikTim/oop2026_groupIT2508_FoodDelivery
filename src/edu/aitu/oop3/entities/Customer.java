1884a224e7d0d1fe7f7559e19fd4cbddbacb90db
Timur <timurg2qwer123@gmail.com>
2026-01-16 07:16:32 +0500

    Timur Mukhsimov- changed Customer entity

--- src/edu/aitu/oop3/entities/Customer.java
+++ src/edu/aitu/oop3/entities/Customer.java
@@ -4,9 +4,11 @@ public class Customer {
     private int id;
     private String name;
 
+
     public Customer(String name) {
         this.name = name;
     }
+
 
     public Customer(int id, String name) {
         this.id = id;
@@ -15,9 +17,4 @@ public class Customer {
 
     public int getId() { return id; }
     public String getName() { return name; }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
 }
